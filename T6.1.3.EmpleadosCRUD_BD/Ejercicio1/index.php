e648656ef1dadbf573cc431ece3ca2ead2f04997
dadodopa <dopazodominguez@gmail.com>
2026-01-16 14:18:27 +0100

    16/01/2026

---
+++ T6.1.3.EmpleadosCRUD_BD/Ejercicio1/index.php
@@ -0,0 +1,5 @@
+<?php
+// Incluye la clase 
+require_once '../ConexionBD.php';
+// Crea una instancia de la clase para poder trabajar con el objeto este caso una conexion
+$conexion = new ConexionBD("programacion_ejemplos","Daniel","abc123.","localhost");
